37af9618fa6a75d0e58aa027e54e53d939351007
Ayush04H <ayush050419@gmail.com>
2024-07-07 14:37:49 +0530

    All operations on circular linked list

---
+++ Circular-Linkedlist/circular_linked_list.py
@@ -0,0 +1,122 @@
+class node:
+    def __init__(self,key):
+        self.key=key
+        self.next=None
+
+# - Author -  Ayush Srivastava
+
+''' Method -1 where we do the insert operation in linear time
+'''
+
+
+''' Method -2 where we do insert in constant time  - O(1)
+'''
+
+def insertbeg_m2(head,k):
+    temp=node(k)
+    if head==None:
+        temp.next=temp
+        return temp
+    else:
+        temp.next=head.next
+        head.next=temp
+        head.key,temp.key=temp.key,head.key
+        return head
+
+def insert_end_m2(head,k):
+    temp=node(k)
+    if head== None:
+        temp.next=temp
+        return temp
+    else:
+        temp.next=head.next
+        head.next=temp
+        temp.key,head.key=head.key,temp.key
+        return temp
+
+
+def insert_pos(head, k, pos):
+    temp = node(k)
+    if head is None and pos == 1:
+        temp.next = temp
+        return temp
+    
+    curr = head
+    for i in range(pos - 2):
+        if curr.next == head:
+            break
+        curr = curr.next
+    
+    temp.next = curr.next
+    curr.next = temp
+    return head
+
+
+def delete_head(head):
+    if head==None:
+        return None
+    elif head.next == head:
+        return None
+    else:
+        head.key=head.next.key
+        head.next=head.next.next 
+        return head
+
+def delete_pos(head,pos):
+    if head==None:
+        return None
+    elif pos==1:
+        delete_head(head)
+    curr=head
+    for i in range(pos-2):
+        curr=curr.next
+    curr.next=curr.next.next
+    return head
+
+def length_of_list(head):
+    c=1
+    if head==None:
+        return 
+    curr=head.next
+    while curr != head:
+        curr=curr.next
+        c+=1
+    return c
+
+def delete_last(head):
+    pos=length_of_list(head)
+    return delete_pos(head,pos)
+
+def printcc(head):
+    if head==None:
+        return 
+    print(head.key,end=" ")
+    curr=head.next
+    while curr != head:
+        print(curr.key,end=" ")
+        curr=curr.next
+
+if __name__ == '__main__':
+
+    # Inserting Data
+    head=node(5)
+    head.next=node(10)
+    head.next.next=node(15)
+    head.next.next.next = node(30)
+    head.next.next.next.next = node(50)
+    head.next.next.next.next.next = head
+    head=insertbeg_m2(head,4)
+    head=insertbeg_m2(head,2)
+    head=insert_end_m2(head,99)
+    head=insert_pos(head,3,2)
+    head=insert_pos(head,7,5)
+
+    printcc(head)
+    print("\n")
+
+    # Deleting Data
+    head=delete_head(head)
+    head=delete_pos(head,2)
+    head=delete_last(head)
+
+    printcc(head)
--- Circular-Linkedlist/test.py
+++ Circular-Linkedlist/test.py
@@ -1,57 +1,113 @@
-class node:
-    def __init__(self,key):
-        self.key=key
-        self.next=None
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
 
-# - Author -  Ayush Srivastava
-
-''' Method -1 where we do the insert operation in linear time
-'''
-def insertbeg_m1(head,k):
-    temp=node(k)
-    if head==None:
-        temp.next=temp
-        return temp
-    curr=head
-    while  curr.next != head:
-        curr=curr.next
-    curr.next=temp
-    temp.next=head
-    return temp
-
-''' Method -2 where we do insert in constant time
-'''
-
-def insertbeg_m2(head,k):
-    temp=node(k)
-    if head==None:
-        temp.next=temp
+# Method to insert at the beginning in constant time
+def insert_beg(head, k):
+    temp = Node(k)
+    if head is None:
+        temp.next = temp
         return temp
     else:
-        temp.next=head.next
-        head.next=temp
-        head.key,temp.key=temp.key,head.key
+        temp.next = head.next
+        head.next = temp
+        head.key, temp.key = temp.key, head.key
         return head
 
+# Method to insert at the end in constant time
+def insert_end(head, k):
+    temp = Node(k)
+    if head is None:
+        temp.next = temp
+        return temp
+    else:
+        temp.next = head.next
+        head.next = temp
+        head.key, temp.key = temp.key, head.key
+        return temp
 
+# Method to insert at a given position in constant time
+def insert_at_position(head, k, pos):
+    temp = Node(k)
+    if head is None and pos == 1:
+        temp.next = temp
+        return temp
+    
+    curr = head
+    for _ in range(pos - 2):
+        if curr.next == head:
+            break
+        curr = curr.next
+    
+    temp.next = curr.next
+    curr.next = temp
+    return head
 
-def printcc(head):
-    if head==None:
-        return 
-    print(head.key,end=" ")
-    curr=head.next
+# Method to delete from the beginning in constant time
+def delete_beg(head):
+    if head is None:
+        return None
+    if head.next == head:
+        return None
+    head.key = head.next.key
+    head.next = head.next.next
+    return head
+
+# Method to delete from the end in constant time
+def delete_end(head):
+    if head is None:
+        return None
+    if head.next == head:
+        return None
+    curr = head
+    while curr.next.next != head:
+        curr = curr.next
+    curr.next = head
+    return head
+
+# Method to delete from a given position in constant time
+def delete_at_position(head, pos):
+    if head is None:
+        return None
+    if pos == 1:
+        return delete_beg(head)
+    curr = head
+    for _ in range(pos - 2):
+        if curr.next.next == head:
+            break
+        curr = curr.next
+    curr.next = curr.next.next
+    return head
+
+def print_cc(head):
+    if head is None:
+        return
+    print(head.key, end=" ")
+    curr = head.next
     while curr != head:
-        print(curr.key,end=" ")
-        curr=curr.next
+        print(curr.key, end=" ")
+        curr = curr.next
+    print()
 
 if __name__ == '__main__':
-    head=node(5)
-    head.next=node(10)
-    head.next.next=node(15)
-    head.next.next.next = node(30)
-    head.next.next.next.next = node(50)
+    head = Node(5)
+    head.next = Node(10)
+    head.next.next = Node(15)
+    head.next.next.next = Node(30)
+    head.next.next.next.next = Node(50)
     head.next.next.next.next.next = head
-    head=insertbeg_m1(head,4)
-    head=insertbeg_m2(head,2)
 
-    printcc(head)
+    head = insert_beg(head, 4)
+    head = insert_end(head, 60)
+    head = insert_at_position(head, 25, 3)
+
+    print("List after insertions:")
+    print_cc(head)
+
+    head = delete_beg(head)
+    head = delete_end(head)
+    head = delete_at_position(head, 4)
+
+    print("List after deletions:")
+    print_cc(head)
