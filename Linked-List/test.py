48825ea2799651c1166ba44991b6e59c4f493aef
Ayush04H <ayush050419@gmail.com>
2024-07-07 21:40:59 +0530

    Practise

--- Linked-List/test.py
+++ Linked-List/test.py
@@ -1,37 +1,79 @@
-class Node:
-    def __init__(self, k):
-        self.key = k
-        self.next = None
+class node:
+    def __init__(self,key):
+        self.key=key
+        self.next=None
+
+def insert_beg(head,k):
+    temp=node(k)
+    temp.next=head
+    return temp
 
 
-def reversel_m2(head):
-    curr = head
-    prev = None
+def insert_end(head,k):
+    if head==None:
+        return node(k) 
+    curr=head 
+    while curr.next != None:
+        curr=curr.next
+    curr.next=node(k)
+    return head 
 
-    while curr is not None:
-        next = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next
+def insert_pos(head,k,pos):
+    if head==None or pos==1:
+        return insert_beg(head,k)
+    curr=head
+    for i in range(pos-2):
+        curr=curr.next
+    temp=node(k)
+    temp.next=curr.next
+    curr.next=temp
+    return head
 
-    return prev
+def delete_beg(head):
+    if head==None:
+        return 
+    n=head.next
+    head.next=None
+    return n
+
+def delete_end(head):
+    if head==None:
+        return
+    if head.next==None:
+        return
+    curr=head
+    while curr.next.next!=None:
+        curr=curr.next
+    curr.next=None
+    return head
+def delete_pos(head,pos):
+    if pos==1:
+        return delete_beg(head)
+    temp=head
+    for i in range(pos-2):
+        temp=temp.next
+    temp.next=temp.next.next
+    return head
+def printhead(head):
+    curr=head
+    while curr!=None:
+        print(curr.key,end=" ")
+        curr=curr.next
+    print("\n")
 
 
-def printList(head):
-    curr = head
-    while curr != None:
-        print(curr.key, end=" ")
-        curr = curr.next
-    print()
+if __name__ == '__main__':
+    head=None
+    head=insert_beg(head,10)
+    head=insert_end(head,100)
+    head=insert_pos(head,20,2)
+    head=insert_pos(head,90,3)
+    head=insert_pos(head,9,1)
+    head=insert_pos(head,99,5)
 
+    print("Printing List after insertion")
+    printhead(head)
 
-head = Node(10)
-head.next = Node(20)
-head.next.next = Node(30)
-head.next.next.next = Node(40)
-
-printList(head)
-
-head = reversel_m2(head)
-
-printList(head)
+    print("Deleting from list\n")
+    head=delete_pos(head,4)
+    printhead(head)
