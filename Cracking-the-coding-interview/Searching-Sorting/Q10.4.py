82892a43353ff75f3ee2c0f5e1bbac1cbdd62875
Ayush04H <ayush050419@gmail.com>
2024-08-01 10:20:16 +0530

    Search pos , No Size

--- Cracking-the-coding-interview/Searching-Sorting/Q10.4.py
+++ Cracking-the-coding-interview/Searching-Sorting/Q10.4.py
@@ -48,3 +48,38 @@ def test_solution():
     print(f"Index of target {target}: {result}")  # Expected output: -1
 
 test_solution()
+
+
+
+def binary_search(arr,l,r,x):
+
+	if r >= l:
+		mid = l+(r-l)//2
+
+		if arr[mid] == x:
+			return mid
+
+		if arr[mid] > x:
+			return binary_search(arr,l,mid-1,x)
+
+		return binary_search(arr,mid+1,r,x)
+
+	return -1
+
+def findPos(a, key):
+
+	l, h, val = 0, 1, arr[0]
+
+	while val < key:
+		l = h		 
+		h = 2*h		 
+		val = arr[h]	
+
+	return binary_search(a, l, h, key)
+
+arr = [-1, 0, 3, 5, 9, 12]
+ans = findPos(arr,9)
+if ans == -1:
+	print ("Element not found")
+else:
+	print("Element found at index",ans)
