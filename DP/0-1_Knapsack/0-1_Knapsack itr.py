aa516f11198038683e91420b74bd30218d459b5d
Ayush04H <ayush050419@gmail.com>
2024-09-01 18:15:19 +0530

    O-1 Knapsack & Its Variation

--- DP/0-1_Knapsack/0-1_Knapsack itr.py
+++ DP/0-1_Knapsack/0-1_Knapsack itr.py
@@ -4,7 +4,7 @@ def knapsack(W: int, val: list[int], weight: list[int], n: int) -> int:
 
     # Step 2: Fill the table
     for i in range(1, n + 1):
-        for w in range(W + 1):
+        for w in range(1,W + 1):
             if weight[i - 1] <= w:
                 # If the current item's weight is less than or equal to the current capacity,
                 # take the maximum of including or not including the item
