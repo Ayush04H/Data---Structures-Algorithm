7384b00b1a3b48e8eefd844b6e9c0cb11d08b552
Ayush04H <ayush050419@gmail.com>
2024-09-02 23:21:52 +0530

    Unbounded Knapsack -- Coin Change Problem : Max number ways

---
+++ DP/unbounded_Knapsack/coin_change_max.py
@@ -0,0 +1,29 @@
+def count_subset( arr:list[int] , Sum:int):
+    n=len(arr)
+    t = [[0 for _ in range(Sum + 1)]for _ in range(n+1)]
+    for i in range(n+1):
+        t[i][0]= 1
+
+    for i in range(1,n+1):
+        for s in range(1,Sum + 1):
+            if arr[i-1] <= s:
+                t[i][s] = t[i][s - arr[i-1]] + t[i -1][s]
+            else:
+                t[i][s] = t[i -1][s]
+
+    return t[n][s]
+
+
+def main():
+    # Example values for the Subset Sum problem
+    arr = [2, 5, 3, 6]  # Set of numbers
+    sum_val = 10  # Desired sum
+    
+    # Call the subset_sum function
+    result = count_subset(arr, sum_val)
+    
+    # Print the result
+    print("Is there a subset with the given sum?", result)
+
+if __name__ == "__main__":
+    main()
