06efb8ffa015f7b8e8c2042df5be2d8901f99dfe
Ayush04H <ayush050419@gmail.com>
2024-09-04 10:45:57 +0530

    Lcs Iterative way

---
+++ DP/LCS/lcs_itr.py
@@ -0,0 +1,21 @@
+def lcs(x :str,y:str) ->int :
+    m = len(x)
+    n = len(y)
+
+    t = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+
+            if x[i-1] == y[j-1]:
+                t[i][j] = 1 + t[i-1][j-1]
+
+            else:
+                t[i][j] = max(t[i-1][j] , t[i][j-1])
+
+    return t[m][n]
+
+S1 = "AGGTABX"
+S2 = "GXTXAYB"
+
+print("Length of LCS is", lcs(S1, S2))
