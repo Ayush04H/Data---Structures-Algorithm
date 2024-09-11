de2de2cd5cb7efc4d6e0f3c775b1753b0e00c92e
Ayush04H <ayush050419@gmail.com>
2024-09-11 09:39:52 +0530

    Modification

--- Graph/BFS.py
+++ Graph/BFS.py
@@ -8,7 +8,8 @@ def print_list(adj):
     for u,v in enumerate(adj):
         print(u,v)
     
-def bfs(adj,s):
+def bfs(adj):
+    s=0
     q=deque()
     q.append(s)
     visited = [False] * len(adj)
@@ -22,16 +23,16 @@ def bfs(adj,s):
                 visited[u]=True
 
 
-v=5
+v=6
 adj=[[] for i in range(v)]
 add_adj(adj,0,1)
-add_adj(adj,0,4)
-add_adj(adj,1,4)
+add_adj(adj,0,2)
+add_adj(adj,0,5)
 add_adj(adj,1,3)
-add_adj(adj,1,2)
-add_adj(adj,2,3)
-add_adj(adj,4,3)
+add_adj(adj,3,5)
+add_adj(adj,4,5)
+add_adj(adj,2,4)
 
 print_list(adj)
-bfs(adj,0)
+bfs(adj)
 
