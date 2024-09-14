7df4a53d892d164d8f15a0783797ffcb9e782485
Ayush04H <ayush050419@gmail.com>
2024-09-14 22:52:00 +0530

    Left and Right Rotate Array by 1 place

--- List/leftrot.py
+++ List/leftrot.py
@@ -1,6 +1,16 @@
-def rot(l):
+def leftrot(l):
     l.append(l.pop(0))
     print(l)
+
+
+def rightrot(l):
+    l.insert(0,l.pop())
+    print(l)
+
+
 if __name__=='__main__':
     l=[1,2,3,4,5,6,7,8]
-    rot(l)
+    leftrot(l)
+
+    l2 =[1,2,3,4]
+    rightrot(l2)
