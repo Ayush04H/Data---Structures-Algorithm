9c5859d1fc330dc00f657c6a3ce7726baeda9595
Ayush04H <ayush050419@gmail.com>
2024-09-15 09:31:25 +0530

    EVEN ODD TWO POINTER

--- List/sepevenodd.py
+++ List/sepevenodd.py
@@ -8,8 +8,28 @@ def evenodd(l):
             l2.append(l[i])
 
     return l1,l2
+
+
+def evenoddtwop(l):
+    i=0 
+    j=len(l)-1
+
+    while i<j:
+        if l[i] %2 !=0:
+            i+=1
+
+        elif l[j] %2 == 0:
+            j-=1
+
+        else:
+            l[i],l[j] = l[j],l[i]
+            i+=1
+            j-=1
+
+    return l
 if __name__=='__main__':
     l=[1,2,3,4,5,6,7,8,9,10,11,2,25,41,89,85,68,58,56,70]
     even,odd=evenodd(l)
     print(even)
     print(odd)
+    print(evenoddtwop(l))
