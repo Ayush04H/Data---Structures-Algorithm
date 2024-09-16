1b46ac31b9de2c4ea4b8cea32229d42315ba89e7
Ayush04H <ayush050419@gmail.com>
2024-09-16 11:01:54 +0530

    Modifications

--- Graph/DFS.py
+++ Graph/DFS.py
@@ -25,12 +25,10 @@ def print_list(adj):
 v=5
 adj=[[] for i in range(v)]
 add_adj(adj,0,1)
-add_adj(adj,0,4)
-add_adj(adj,1,4)
-add_adj(adj,1,3)
+add_adj(adj,0,3)
 add_adj(adj,1,2)
-add_adj(adj,2,3)
-add_adj(adj,4,3)
+add_adj(adj,1,3)
+add_adj(adj,3,2)
 
 print_list(adj)
 DFS_rec(adj,0)
