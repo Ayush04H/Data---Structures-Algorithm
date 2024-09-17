fe8f9dc97d52ecd9d0ae5d27bdf36d6d719eba92
Ayush04H <ayush050419@gmail.com>
2024-09-17 15:29:09 +0530

    Prims Adj List

--- Graph/prims_adj_list.py
+++ Graph/prims_adj_list.py
@@ -1,16 +1,3 @@
-def print_mst(parent, graph, V):
-    total_weight = 0
-    print("Edge \tWeight")
-    for i in range(1, V):
-        u = parent[i]
-        v = i
-        # Find the weight of the edge u-v in the adjacency list
-        weight = next(weight for node, weight in graph[u] if node == v)
-        print(f"{u} - {v} \t {weight}")
-        total_weight += weight
-
-    print(f"Total weight of MST: {total_weight}")
-
 def minimum_key(key, V, mstset):
     min_index = -1
     min_value = float('inf')
@@ -24,21 +11,22 @@ def minimum_key(key, V, mstset):
 
 def prims(graph, V):
     key = [float('inf')] * V
-    parent = [None] * V
     mstset = [False] * V
-    parent[0] = -1
     key[0] = 0
+    res = 0
 
     for _ in range(V):
         u = minimum_key(key, V, mstset)
         mstset[u] = True
 
-        for v, weight in graph[u]:
-            if not mstset[v] and key[v] > weight:
-                key[v] = weight
-                parent[v] = u
+        res += key[u]
 
-    print_mst(parent, graph, V)
+        # Iterate over the adjacency list for the vertex `u`
+        for (s, weight) in graph[u]:
+            if not mstset[s] and weight != 0 and weight < key[s]:
+                key[s] = weight
+
+    return res
 
 if __name__ == '__main__':
     V = 4
@@ -53,5 +41,6 @@ if __name__ == '__main__':
     graph[2].append((3, 20))
     graph[3].append((1, 15))
     graph[3].append((2, 20))
-
-    prims(graph, V)
+    
+    result = prims(graph, V)
+    print("Total weight of the minimum spanning tree:", result)
