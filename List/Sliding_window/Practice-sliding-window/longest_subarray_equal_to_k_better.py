21745f4f9d27817e74815d611c90fc05a0a1df20
Ayush04H <ayush050419@gmail.com>
2024-10-09 16:00:02 +0530

    Longest Subarray Concept

---
+++ List/Sliding_window/Practice-sliding-window/longest_subarray_equal_to_k_better.py
@@ -0,0 +1,29 @@
+def longest_subarray_equal_k(arr,k):
+    max_len = 0
+    sum_tot = 0
+    res = []
+    i,j = 0,0 
+    n = len(arr)
+
+    while j < n:
+        sum_tot += arr[j]
+
+        while sum_tot > k and i <= j:
+            sum_tot -= arr[i]
+            i += 1
+
+        if sum_tot == k:
+            if j - i + 1 > max_len:
+                max_len = j -i + 1
+                res = arr[i:j+1]
+        j+=1
+
+    return max_len , res 
+ 
+
+arr = [1, 2, 3, 1, -1, 3, 5, 2]
+# print(generate_subarray(arr))
+
+max_len , res = longest_subarray_equal_k(arr,10)
+print('Size of the array is ',max_len)
+print('The Subarray is ',res)
