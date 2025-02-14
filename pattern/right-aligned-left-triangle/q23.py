f277512ba2f2e5ccad5eab7ce110b11615f3bd5b
Ayush04H <ayush050419@gmail.com>
2025-02-14 12:52:39 +0530

    Right Aligned Right Triangle Pattern

---
+++ pattern/right-aligned-left-triangle/q23.py
@@ -0,0 +1,20 @@
+'''
+    1234
+     234
+      34
+       4
+'''
+
+def func(n):
+    for i in range(n,0,-1):
+        for j in range(1,n+1):
+            if j<=n-i:
+                print(" ",end="")
+
+            else:
+                print(chr(j+64),end="")
+
+        print()
+
+
+func(4)
