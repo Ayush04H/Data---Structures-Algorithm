e5740801a2231b6bc978c429405f368e8ceb7879
Ayush04H <ayush050419@gmail.com>
2025-02-14 12:48:24 +0530

    Left Aligned Right Triangle Pattern

---
+++ pattern/left-aligned-right-triangle/q7.py
@@ -0,0 +1,24 @@
+'''
+*
+**
+***
+****
+***
+**
+*
+'''
+
+def func(n:int):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print('*',end="")
+
+        print("")
+
+    for i in range(n-1,0,-1):
+        for j in range(1,i+1):
+            print('*',end="")
+
+        print("")
+
+func(4)
