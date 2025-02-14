4f2e4b291e54ed830d3152d63cf900ea9c3cb5b9
Ayush04H <ayush050419@gmail.com>
2025-02-14 14:53:25 +0530

    Equilateral Triangle Pattern

---
+++ pattern/Equilateral-triangle/q33.py
@@ -0,0 +1,24 @@
+'''
+    A    
+   ABA   
+  ABCBA  
+ ABCDCBA 
+ABCDEDCBA
+'''
+
+def func(n):
+    # Upper triangle
+    for i in range(1, n + 1):
+        num = 1
+        for j in range(1, 2 * n):
+            if j >= n - (i - 1) and j <= n + (i - 1):
+                print(chr(num+64), end="")
+                if j < n:
+                    num += 1
+                else:
+                    num -= 1
+            else:
+                print(" ", end="")
+        print()
+
+func(5)
