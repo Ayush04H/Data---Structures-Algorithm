557e9b1fdfbaa3729df251bc74ca92476d94f4e5
Ayush04H <ayush050419@gmail.com>
2025-02-14 14:53:15 +0530

    Equilateral Triangle Pattern

---
+++ pattern/Equilateral-triangle/q32.py
@@ -0,0 +1,20 @@
+'''
+555555555
+ 4444444 
+  33333  
+   222   
+    1    
+'''
+
+def func(n):
+    for i in range(n,0,-1):
+        for j in range(1,2*n):
+            if j >= n-(i-1) and j <= n+(i-1):
+                print(chr(i+64),end="")
+            else:
+                print(" ",end="")
+
+        print()
+
+
+func(5)
